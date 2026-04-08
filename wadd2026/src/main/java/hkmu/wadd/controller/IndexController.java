d0d749dffd9795ec8cee87dd02d4b2d51f3a304c
benkwok123 <guochingleung@gmail.com>
2026-04-08 08:36:41 +0800

    Update IndexController.java

--- wadd2026/src/main/java/hkmu/wadd/controller/IndexController.java
+++ wadd2026/src/main/java/hkmu/wadd/controller/IndexController.java
@@ -2,6 +2,7 @@ package hkmu.wadd.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
@@ -13,6 +14,12 @@ public class IndexController {
 
     @GetMapping("/login")
     public String loginPage() {
-        return "login"; // resolves to /WEB-INF/jsp/login.jsp
+        return "login";
     }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+
 }
